edcd06936f1e1c62ac8373db8f71730d6bb227b0
Poetic-dream124 <2074759915@qq.com>
2025-09-11 16:32:10 +0800

    Site updated: 2025-09-11 16:32:05

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1757508035301";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1757579519925";const preCache = ["/images/taichi.png","/images/V88020_00.png","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
